36a73d70324bd7d1d3cccdb1adaa1a8467beeb34
Will Flores <waflores956@gmail.com>
2026-04-25 09:43:05 -0400

    We are adding an entry for our Legion Laptop MT: 83F3.

--- kernel_module/legion-laptop.c
+++ kernel_module/legion-laptop.c
@@ -1026,6 +1026,25 @@ static const struct model_config model_nrcn = {
 	.ramio_size = 0x600
 };
 
+// Legion Legion Pro 5i Gen 10 Intel with RTX 5070 - Model 83F3
+static const struct model_config model_q6cn = {
+	.registers = &ec_register_offsets_v0,
+	.check_embedded_controller_id = true,
+	.embedded_controller_id = 0x5508,
+	.memoryio_physical_ec_start = 0xC400,
+	.memoryio_size = 0x300,
+	.has_minifancurve = true,
+	.has_custom_powermode = true,
+	.access_method_powermode = ACCESS_METHOD_WMI,
+	.access_method_keyboard = ACCESS_METHOD_WMI,
+	.access_method_fanspeed = ACCESS_METHOD_WMI3,
+	.access_method_temperature = ACCESS_METHOD_WMI3,
+	.access_method_fancurve = ACCESS_METHOD_WMI3,
+	.access_method_fanfullspeed = ACCESS_METHOD_WMI,
+	.acpi_check_dev = true,
+	.ramio_physical_start = 0xFE0B0400,
+	.ramio_size = 0x600
+};
 
 static const struct dmi_system_id denylist[] = { {} };
 
@@ -1420,6 +1439,16 @@ static const struct dmi_system_id optimistic_allowlist[] = {
 			DMI_MATCH(DMI_BIOS_VERSION, "NRCN"),
 		},
 		.driver_data = (void *)&model_nrcn
+	},
+	{
+		// e.g. Legion Legion Pro 5i Gen 10 Intel (16") with RTX 5070
+		// Part Number: 83F3000HUS
+		.ident = "Q6CN",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
+			DMI_MATCH(DMI_BIOS_VERSION, "Q6CN"),
+		},
+		.driver_data = (void *)&model_q6cn
 	},
 	{}
 };
